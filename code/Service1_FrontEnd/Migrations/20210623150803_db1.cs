9386bb0a43131393eb39ff7e4301f371571eb759
asma-syed <asma.syed@avanade.com>
2021-06-23 16:11:54 +0100

    updateddb1

---
+++ code/Service1_FrontEnd/Migrations/20210623150803_db1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Service1_FrontEnd.Migrations
+{
+    public partial class db1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aliases",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: true),
+                    Surname = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aliases", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aliases");
+        }
+    }
+}
